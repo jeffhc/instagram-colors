e905d727e6c99676e6855d4e653fd8095dc811c2
zacmustin <zachary_mustin18@milton.edu>
2018-04-24 14:09:27 -0400

    Model is w0rKinG1!

--- model/train.py
+++ model/train.py
@@ -7,53 +7,54 @@ import functools
 import imageio
 import pickle
 import sys
+import piexif, piexif.helper, json
 from os import listdir, mkdir
 from os.path import isfile, join, exists
 
 VGG_PATH = 'imagenet-vgg-verydeep-19.mat'
 IMAGE_DATA_DIR = '../cleaning/scaled_photos'
 TOTAL_IMAGES = 12
-BATCH_SIZE = 3
+BATCH_SIZE = 4
 LEARNING_RATE = 1e-3
 
 
 def load_photos(array):
-    # Takes a list of filenames, returns a dictionary with np.arrays() and metadata
-    data = {
-        "images": [],
-        "likes": []
-    }
-    for filepath in array:
-        try:
-            img = Image.open(filepath)
-            img.load()
-            rgb_data = np.asarray( img, dtype="int32" )
-            data["photos"].append(rgb_data)
+	# Takes a list of filenames, returns a dictionary with np.arrays() and metadata
+	data = {
+		"images": [],
+		"likes": []
+	}
+	for filepath in array:
+		#try:
+		img = Image.open(filepath)
+		img.load()
+		rgb_data = np.asarray( img, dtype="int32" )
+		data["images"].append(rgb_data)
 
-            exif_dict = piexif.load(filepath)
-            user_comment = piexif.helper.UserComment.load(exif_dict["Exif"][piexif.ExifIFD.UserComment])
-            metadata = json.loads(user_comment)
-            
-            data["likes"].append(metadata["likes"])
-            #all_data["ratio"].append(metadata["ratio"])
-            
-        except:
-            print("Could not load: " + filepath)
-            with open('train_error_log.txt', 'a') as f:
-                f.write("Could not load: " + filepath + '\n')
-    return data
+		exif_dict = piexif.load(filepath)
+		user_comment = piexif.helper.UserComment.load(exif_dict["Exif"][piexif.ExifIFD.UserComment])
+		metadata = json.loads(user_comment)
+		
+		data["likes"].append(metadata["likes"])
+			#all_data["ratio"].append(metadata["ratio"])
+			
+		#except:
+		 #   print("Could not load: " + filepath)
+		  #  with open('train_error_log.txt', 'a') as f:
+		   #	 f.write("Could not load: " + filepath + '\n')
+	return data
 
 def chunks(array, chunk_size, mode):
-    """Yield successive n-sized chunks from l."""
-    for i in range(0, len(array), chunk_size):
-        data = load_photos(array[i:i + chunk_size])
-        yield data[mode]
+	"""Yield successive n-sized chunks from l."""
+	for i in range(0, len(array), chunk_size):
+		data = load_photos(array[i:i + chunk_size])
+		yield data[mode]
 
 def weights_variable(shape, name):
-    return tf.Variable(tf.truncated_normal(shape=shape), name=name)
+	return tf.Variable(tf.truncated_normal(shape=shape), name=name)
 
 def biases_variable(shape, name):
-    return tf.Variable(tf.constant(0.1, shape=shape), name=name)
+	return tf.Variable(tf.constant(0.1, shape=shape), name=name)
 
 
 # Placeholder variables
@@ -66,22 +67,21 @@ likes = tf.placeholder(tf.float32, shape=(None, 1), name='likes')
 #images_vgg = net['relu5_4'] # TODO: Has the image been through the VGG_net?
 #print(images_vgg.shape)
 
-
 # Take last layer of net and convolve it
 def general_convolution(input, layer_name):
-    "Performs a 5x5 convolution on `input` with a stride length of 2 (halves input image size)"
-    "Input and output channels are both 1"
-    weights = weights_variable([5, 5, 3, 3], '%s_weights' % layer_name)
-    convolution = tf.nn.conv2d(input, filter=weights, strides=[1, 2, 2, 3], padding='SAME')
+	"Performs a 5x5 convolution on `input` with a stride length of 2 (halves input image size)"
+	"Input and output channels are both 1"
+	weights = weights_variable([5, 5, 3, 3], '%s_weights' % layer_name)
+	convolution = tf.nn.conv2d(input, filter=weights, strides=[1, 2, 2, 1], padding='SAME')
 
-    output_width = int(convolution.shape[1])
-    output_height = int(convolution.shape[2])
+	output_width = int(convolution.shape[1])
+	output_height = int(convolution.shape[2])
 
-    biases = biases_variable([output_width, output_height, 1], '%s_biases' % layer_name)
+	biases = biases_variable([output_width, output_height, 1], '%s_biases' % layer_name)
 
-    return tf.nn.relu(convolution + biases)
+	return tf.nn.relu(convolution + biases)
 
-# Convolve: After convolving, shape is (40, 40, 3)
+# Convolve: After convolving, shape is (?, 40, 40, 3)
 relu1 = general_convolution(images, 'relu1')
 relu2 = general_convolution(relu1, 'relu2')
 relu3 = general_convolution(relu2, 'relu3')
@@ -98,10 +98,7 @@ fc0 = tf.nn.relu(tf.matmul(relu4, fc_weights0) + fc_biases0)
 
 fc_weights1 = tf.Variable(tf.truncated_normal([int(fc0.shape[1]), 1]), name='fc_weights1')
 fc_biases1 = tf.Variable(tf.constant(0.1, shape=(1,)), name='fc_biases1')
-
 output_likes = tf.matmul(fc0, fc_weights1) + fc_biases1
-output_likes = tf.reshape(output_likes, [-1, 1])
-
 
 # Define cost
 cost = tf.losses.mean_squared_error(output_likes, likes) # TODO: Does order matter?
@@ -121,29 +118,32 @@ all_file_names = [join(IMAGE_DATA_DIR, f) for f in listdir(IMAGE_DATA_DIR) if is
 
 for epoch in range(100000):
 
-    
-    for train_images, train_likes in zip(chunks(all_file_names, BATCH_SIZE, 'images'), chunks(all_file_names, BATCH_SIZE, 'likes')):
-        sess.run(train, feed_dict={images: train_images, likes: train_likes})
+	for train_images, train_likes in zip(chunks(all_file_names, BATCH_SIZE, 'images'), chunks(all_file_names, BATCH_SIZE, 'likes')):
+		train_likes = np.array(train_likes)
+		train_likes = np.reshape(train_likes, [len(train_likes), 1])
+		sess.run(train, feed_dict={images: train_images, likes: train_likes})
 
-    if epoch % 100 == 0:
-        # Create checkpoint
-        saver.save(sess, './tensorboard/checkpoint', global_step=epoch)
+	if epoch % 100 == 0:
+		# Create checkpoint
+		saver.save(sess, './tensorboard/checkpoint', global_step=epoch)
 
-        # Evaluate Performance
-        print('Epoch: %s of 100000' % epoch)
-        print('Cost: %s' % sess.run(cost, feed_dict={images: train_images, likes: train_likes}))
-        
-        # Save results images
-        results = sess.run(output_likes, feed_dict={images: train_images})
-        for i in range(10): # TODO: Why is this loop here?
-            result_likes = results[i]
+		# Evaluate Performance
+		print('Epoch: %s of 100000' % epoch)
+		print('Cost: %s' % sess.run(cost, feed_dict={images: train_images, likes: train_likes}))
+		
+		# Save results images
+		results = sess.run(output_likes, feed_dict={images: train_images})
+		print(results)
+		#for i in range(10): # TODO: Why is this loop here?
+		#	result_likes = results[i]
 
-            # Save results and originals
-            try:
-                mkdir('results/epoch%s' % epoch)
-            except:
-                pass
-            with open('results/epoch%s/RESULT%s.txt' % (epoch, i), 'a') as f:
-            	f.write(result_likes + '\n')
-            with open('results/epoch%s/ORIGINAL%s.txt' % (epoch, i), 'a') as f:
-            	f.write(train_likes[i] + '\n')
+			# Save results and originals
+		#	try:
+		#		mkdir('results')
+		#		mkdir('results/epoch%s' % epoch)
+		#	except:
+		#		pass
+		#	with open('results/epoch%s/RESULT%s.txt' % (epoch, i), 'a') as f:
+		#		f.write(result_likes + '\n')
+		#	with open('results/epoch%s/ORIGINAL%s.txt' % (epoch, i), 'a') as f:
+		#		f.write(train_likes[i] + '\n')
